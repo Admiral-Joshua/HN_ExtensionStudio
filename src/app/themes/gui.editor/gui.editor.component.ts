45978c71d9354abeb3d9a6449fa9a2e090075043
LeilaSoya <admiraljrn@outlook.com>
2020-10-04 23:15:31 +0100

    Updates to the Theme Editor GUI Some of the values now map and update in the preview window. Some work required to enable transparency configuration. Further mappings required to complete integration. API missing ability to upload theme background.

--- src/app/themes/gui.editor/gui.editor.component.ts
+++ src/app/themes/gui.editor/gui.editor.component.ts
@@ -1,19 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { faCog, faSave, faTimes, faWrench } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { HN_Theme } from '../models';
+import { ThemesService } from '../themes.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-    templateUrl: './gui.editor.component.html',
-    styleUrls: ['./gui.editor.component.css', './hn.gui.layout/hn.display.style.css', './hn.gui.layout/hn.button.style.css']
+  templateUrl: './gui.editor.component.html',
+  styleUrls: ['./gui.editor.component.css', './hn.gui.layout/hn.display.style.css', './hn.gui.layout/hn.button.style.css']
 })
-export class ThemeGuiEditorComponent {
+export class ThemeGuiEditorComponent implements OnInit {
 
-    constructor() { }
+  closeIcon = faTimes;
+  settingsIcon = faCog;
+  saveIcon = faSave;
+  emailIcon = faEnvelope;
+  editIcon = faWrench;
+
+  themeData: HN_Theme = new HN_Theme();
+
+  opacitySettings: any = {
+    moduleBacking: 0.25
+  };
+
+  themeId = 0;
+
+  constructor(@Inject(MAT_DIALOG_DATA) private data: any, private service: ThemesService, private dialogRef: MatDialogRef<ThemeGuiEditorComponent>) {
+    if (this.data.themeId) {
+      this.themeId = this.data.themeId;
+    }
+  }
+
+  ngOnInit() {
+    if (this.themeId > 0) {
+      this.service.fetchTheme(this.themeId).subscribe((themeData) => {
+        this.themeData = themeData;
+      });
+    }
+  }
+
+  saveTheme() {
+    if (this.themeId > 0) {
+      this.service.updateTheme(this.themeId, this.themeData).subscribe(() => {
+        this.dialogRef.close();
+      });
+    } else {
+      this.service.createTheme(this.themeData).subscribe(() => {
+        this.dialogRef.close();
+      });
+    }
+  }
 
 
-    closeIcon = faTimes;
-    settingsIcon = faCog;
-    saveIcon = faSave;
-    emailIcon = faEnvelope;
-    editIcon = faWrench;
 }
